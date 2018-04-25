6feb9c2ff4dd27ebb759562759840c9e45eaf9b0
Ashvat Bansal <ashvatb2@illinois.edu>
2018-04-25 15:43:38 -0500

    App

---
+++ app/src/main/java/edu/illinois/cs/cs125/lyrics/DisplayLyrics.java
@@ -0,0 +1,13 @@
+package edu.illinois.cs.cs125.lyrics;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DisplayLyrics extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_lyrics);
+    }
+}
--- app/src/main/java/edu/illinois/cs/cs125/lyrics/MainActivity.java
+++ app/src/main/java/edu/illinois/cs/cs125/lyrics/MainActivity.java
@@ -2,12 +2,84 @@ package edu.illinois.cs.cs125.lyrics;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 
+import org.json.JSONObject;
+
+public final class MainActivity extends AppCompatActivity {
+    /** Default logging tag for messages from the main activity. */
+    private static final String TAG = "Lyrics: Main";
+
+    /** Request queue for our network requests. */
+    private static RequestQueue requestQueue;
+
+    /**
+     * Run when our activity comes into view.
+     *
+     * @param savedInstanceState state that was saved by the activity last time it was paused
+     */
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Set up a queue for our Volley requests
+        requestQueue = Volley.newRequestQueue(this);
+
+        // Load the main layout for our activity
         setContentView(R.layout.activity_main);
+
+        // Attach the handler to our UI button
+        final Button startAPICall = findViewById(R.id.button);
+        startAPICall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                Log.d(TAG, "Start API button clicked");
+                startAPICall();
+            }
+        });
+
+        // Make sure that our progress bar isn't spinning and style it a bit
+        ProgressBar progressBar = findViewById(R.id.progressBar);
+        progressBar.setVisibility(View.INVISIBLE);
+    }
+
+    /**
+     * Make an API call.
+     */
+    void startAPICall() {
+        try {
+            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                    Request.Method.GET,
+                    "https://api.lyrics.ovh/v1/artist/title",
+                    null,
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(final JSONObject response) {
+                            final TextView helloTextView = (TextView) findViewById(R.id.displayLyrics);
+                            helloTextView.setText(response.toString());
+                            Log.d(TAG, response.toString());
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(final VolleyError error) {
+                    Log.w(TAG, error.toString());
+                }
+            });
+            requestQueue.add(jsonObjectRequest);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
